20d7819b7c61a3e4789e6b93c356d5f8b9e802ed
zeroest <husheart@naver.com>
2021-09-14 02:34:45 +0900

    Basic fail test - JOB_EXECUTION

--- reference/BATCH_JOB_EXECUTION.md
+++ reference/BATCH_JOB_EXECUTION.md
@@ -7,4 +7,9 @@
 
 JOB_EXECUTION과 JOB_INSTANCE는 부모 자식 관계
 
+---
+
+![batch-fail_test](./spring_batch-fail.png)
+
 JOB_EXECUTION은 JOB_INSTANCE가 성공/실패 했던 모든 내역을 갖고 있다.
+
--- src/main/java/me/zeroest/spring_batch/job/SimpleJobConfiguration.java
+++ src/main/java/me/zeroest/spring_batch/job/SimpleJobConfiguration.java
@@ -27,6 +27,7 @@ public class SimpleJobConfiguration {
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
@@ -35,7 +36,17 @@ public class SimpleJobConfiguration {
     public Step simpleStep1(@Value("#{jobParameters[nowDateTime]}") String nowDateTime) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet(((contribution, chunkContext) -> {
-                    log.info(">>>>> This is Step1");
+                    throw new IllegalArgumentException("step1에서 실패");
+                }))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[nowDateTime]}") String nowDateTime) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
                     log.error(">>>>> nowDateTime = {}", nowDateTime);
                     return RepeatStatus.FINISHED;
                 }))
