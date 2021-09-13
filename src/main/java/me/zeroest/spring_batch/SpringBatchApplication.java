f61f9bcecb01d27a3fb98195bbe2b1c5cb90c510
zeroest <husheart@naver.com>
2021-09-14 02:08:55 +0900

    Set up

---
+++ src/main/java/me/zeroest/spring_batch/SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package me.zeroest.spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+
+}
