bdb6be41b0225dfba10a907d92ca0f4fe71071f3
zeroest <husheart@naver.com>
2021-10-08 23:25:42 +0900

    Rearrange

---
+++ inflearn/src/main/java/me/zeroest/batch_inflearn/BatchInflearnApplication.java
@@ -0,0 +1,13 @@
+package me.zeroest.batch_inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchInflearnApplication.class, args);
+    }
+
+}
---
+++ inflearn/src/test/java/me/zeroest/batch_inflearn/BatchInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeroest.batch_inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
